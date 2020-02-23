8c6b3567cd7739b869df73207d4e32a5eae21539
YuHua96 <57917832+YuHua96@users.noreply.github.com>
2020-02-22 20:11:29 -0800

    With hysteresis

--- line_detect/src/main.cpp
+++ line_detect/src/main.cpp
@@ -1,36 +1,47 @@
 #include <Arduino.h>
 // define variable here
  
-#define linedetector       22
+#define linedetector          22
 #define LinethresholdLOW      800
-#define LinethresholdHIGH     1000
+#define LinethresholdHIGH     830
+#define Errorthreshold        1000
 
 const int ledPin = LED_BUILTIN;
 u_int16_t voltageReader=0;
 
-void readLinedetector(void);
+bool eventLineDetected(void);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
-  pinMode(linedetector, INPUT);
   Serial.begin(96000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  readLinedetector();
-}
-
-void readLinedetector(void){
-  voltageReader = analogRead(linedetector);
-  // Serial.println(voltageReader);
-  if(voltageReader>LinethresholdLOW && voltageReader<LinethresholdHIGH){
-    Serial.println(voltageReader);
+  if(eventLineDetected()){
     Serial.println("Watch out, You reach the boundary!'");
   }
-  else{
-    // Serial.println("I am free");
+}
+
+bool eventLineDetected(void){
+  voltageReader = analogRead(linedetector);
+  //Serial.println(voltageReader);
+  static int lineThres = LinethresholdHIGH;
+  static int prevVol = 0;
+  bool event = false;
+  if(voltageReader > Errorthreshold){
+    Serial.println("Error!");
+    return false;
   }
+  if(voltageReader > lineThres && prevVol <= lineThres){
+    event = true;
+    lineThres = LinethresholdLOW;
+  }
+  if(voltageReader <= lineThres && prevVol > lineThres){
+    lineThres = LinethresholdHIGH;
+  }
+  prevVol = voltageReader;
+  return event;
 }
